1f6c978d09843b863781b6ebacd62bd225bf21e9
akabishau <aleksey.kabishau@icloud.com>
2024-01-06 12:55:42 -0500

    update the project structure

--- ruby_language/basics/hashes.rb
+++ ruby_language/basics/hashes.rb
@@ -23,3 +23,14 @@ person.delete(:city)
 person.each do |key, value|
   puts "#{key}: #{value}"
 end
+
+person.keys # [:name, :age, :occupation]
+person.key?(:name) # true
+person.values # ["John", 31, "Software Engineer"]
+person.value?("John") # true
+person.values_at(:name, :age) # ["John", 31]
+
+person.length # 3
+
+person.store(:hobby, "Fishing") # adds a new key-value pair
+person.merge!({ hobby: "Fishing", city: "New York" }) # adds a new key-value pair and updates the value of an existing key
