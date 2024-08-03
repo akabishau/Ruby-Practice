1b931b0ccc9316f1f93d6edf80c7ce256f31ddb1
akabishau <aleksey.kabishau@icloud.com>
2024-08-03 12:07:12 -0400

    reviewed and updated booleans and symbols

--- ruby_language/basics/boolean.rb
+++ ruby_language/basics/boolean.rb
@@ -1,18 +1,29 @@
 # && - AND operator
 # || - OR operator
+# ! - NOT operator
 # != - NOT operator, == - EQUAL operator
 # >, <, >=, <=
+# <=> - Combined Comparison Operator
+
+true.class # => TrueClass - why?
 
 # method? - method that returns true or false
 
 # Examples of popular Ruby methods that return boolean values:
-# 1. empty? - checks if a string, array, or hash is empty
-# 2. include? - checks if an array or string includes a specific element or substring
-# 3. nil? - checks if an object is nil
-# 4. instance_of? - checks if an object is an instance of a specific class
-# 5. respond_to? - checks if an object responds to a specific method
-# 6. is_a? - checks if an object is an instance of a specific class or its subclasses
-# 7. kind_of? - similar to is_a?, checks if an object is an instance of a specific class or its subclasses
-# 8. eql? - checks if two objects are equal in value and type
-# 9. equal? - checks if two objects are the same object
-# 10. frozen? - checks if an object is frozen
+# empty? - checks if a string, array, or hash is empty
+# include? - checks if an array or string includes a specific element or substring
+# nil? - checks if an object is nil
+# instance_of? - checks if an object is an instance of a specific class
+# respond_to? - checks if an object responds to a specific method
+# is_a? - checks if an object is an instance of a specific class or its subclasses
+# kind_of? - similar to is_a?, checks if an object is an instance of a specific class or its subclasses
+# frozen? - checks if an object is frozen
+
+# checks if two objects are equal in value and type
+"value".eql?("value") # true
+"value".eql?("Value") # false
+"value".eql?(:value) # false
+
+# checks if two objects are the same object
+"some".equal?("some") # false
+:some.equal?(:some) # true
--- ruby_language/basics/symbols.rb
+++ ruby_language/basics/symbols.rb
@@ -1,13 +1,46 @@
-# Creating a hash with symbols as keys
-person = { name: "John", age: 30, city: "New York" } # current "json" like way
-# person2 = { :name => "Jonh", :age => 30, :city => "New York"} # Old way
-# => hash rocket
+# can't be edited
+# lowercase, underscore, no spaces
 # can't use integers as symbols
 
-# Accessing values using symbols as keys
-puts person[:name] # Output: "John"
-puts person[:age]  # Output: 30
-puts person[:city] # Output: "New York"
+ # Old way
+ # => hash rocket
+old_person = { :name => "Jonh", :age => 30 }
 
-puts :name.object_id # keeps the same object_id
-puts :name.class # Symbol
+
+# Creating a hash with symbols as keys
+person = { name: "John", age: 30, city: "New York" } # current "json" like way
+person[:name] # Output: "John"
+person.keys.first.class # Symbol
+
+# RAILS not RUBY feature: interchangeble with strings
+# person["name"] # Output: "John" - Rails allows to use strings as keys
+
+
+"test".object_id # 60
+"test".object_id # 80 - different object_id even for the same string
+:name.object_id # keeps the same object_id (reference) - immutable
+:name.class # Symbol
+
+
+name = "ruby"
+symbol_name = :ruby
+name == symbol_name # false
+name.to_sym == symbol_name # true
+
+
+# RAILS
+options = { format: :json, status: :ok }
+
+def user_params
+  params.require(:user).permit(:name, :email)
+end
+
+# symbols or routes => table names, methods
+# models => has_many, belogns_to...
+
+# inside the model -
+def update_attribute(attribute, value)
+  self.send("#{attribute}=", value)
+  self.save
+end
+user.send(:name, "New Name")
